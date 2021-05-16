fab65304a06839abd9ae354b7fdbab0be4a8e208
NilsS <n.steinz@student.vu.nl>
2021-05-16 20:20:18 +0200

    removed useless stuff and finished the docs

--- repeated_CV_builder.py
+++ repeated_CV_builder.py
@@ -1,13 +1,11 @@
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.feature_selection import SelectKBest, RFECV, RFE, chi2
-from sklearn.model_selection import GridSearchCV, StratifiedShuffleSplit
-import sklearn.feature_selection as feature_selection
-from sklearn.pipeline import Pipeline
-from sklearn.metrics import accuracy_score, precision_score, recall_score
-import pandas as pd
 import numpy as np
-from sklearn import linear_model
+import pandas as pd
 import sklearn
+from sklearn import linear_model
+from sklearn.feature_selection import SelectKBest, chi2
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn.model_selection import GridSearchCV, StratifiedShuffleSplit
+from sklearn.pipeline import Pipeline
 
 
 class DCV():
@@ -21,36 +19,37 @@ class DCV():
     cv_outer = StratifiedShuffleSplit(n_splits=outer_repeats, test_size=1 / outer_repeats)
     cv_inner = StratifiedShuffleSplit(n_splits=inner_repeats, test_size=1 / inner_repeats)
 
-    def __init__(self, Model: sklearn.base.BaseEstimator):
+    def __init__(self, Model: sklearn.base.BaseEstimator or sklearn.base.ClassifierMixin):
         self.Model = Model
         self.hyperParams = dict()
         self.selected_features = []
         self.reset_model_results()
 
     def reset_model_results(self):
-        self._best_score = 0
-        self._best_model = None
-        self._best_features = None
-        self._best_params = None
-        self._best_score = 0
-        self.models = []
+        """
+        creates all variables used and resets them if necessary.
+        :return:
+        """
         self.features = []
         self.indexes = None
         self.train_accuracies = []
-        self.all_features = []
         self.train_precision = []
         self.train_recall = []
         self.test_accuracy = []
         self.test_precision = []
         self.test_recall = []
-        self.all_params = []
+        self.all_features = []
 
-    def train_fit(self, data, classes, loop=1, verbose=False):
+    def train_fit(self, data: pd.DataFrame, classes: pd.DataFrame or pd.Series or pd.Index,
+                  loop: int = 1, verbose: bool = False):
         """
+        the main functions that does the repeated cross validation and does the feature selection.
+        first it creates the parameters search space.
+        after which it will start doing the repeated cross validation with a stratified shuffle split.
 
-        :param data:
-        :param classes:
-        :param loop:
+        :param data: the data that the model need to train on. prefferd to just be a pandas Dataframe
+        :param classes: the classification of the data. this can be a row or the index of the data DataFrame
+        :param loop: nr of repeated CV that will be done
         :param verbose:
         :return:
         """
@@ -65,52 +64,43 @@ class DCV():
         self.update_progress(0)
         for i in range(loop):
             for j, (outer_train_index, outer_test_index) in enumerate(self.cv_outer.split(data, classes)):
+                # splits all the data in to test and training
                 outer_train_x, outer_test_x = data.iloc[outer_train_index], data.iloc[outer_test_index]
                 outer_train_y, outer_test_y = classes.iloc[outer_train_index], classes.iloc[outer_test_index]
-                #
-
+                # creates the pipeline for feature selection and classification
                 pipeline = Pipeline([("feature_selection", SelectKBest(chi2, k=self.num_features)),
                                      ("classification", self.Model)])
-
                 search = GridSearchCV(pipeline, self.hyperParams_est, scoring=self.__scoring__, cv=self.cv_inner,
                                       n_jobs=4, refit="accuracy")
-
+                # fits the data in to the pipeline
                 search.fit(outer_train_x + 1, outer_train_y.to_numpy().ravel())
                 feature_selection = search.best_estimator_.named_steps['feature_selection']
-
+                # saves the training data
                 self.train_accuracies += [search.cv_results_["mean_test_accuracy"]]
                 self.train_precision += [search.cv_results_["mean_test_precision"]]
                 self.train_recall += [search.cv_results_["mean_test_recall"]]
-
-                # print(self.all_accuracies)
+                # tests the best model out of the grid search on the test data
                 self.__test__(search, outer_test_x, outer_test_y)
-                if self.test_accuracy[-1][0] > self._best_score:
-                    self._best_model = search.best_estimator_
-                    self._best_params = [search.best_params_]
-                    self._best_score = self.test_accuracy[-1][0]
-                    self._best_features = feature_selection.transform(np.arange(len(data.columns)).reshape(1, -1))
-                elif self.test_accuracy[-1][0] == self._best_score:
-                    self._best_params += [search.best_params_]
-                    self._best_features += feature_selection.transform(np.arange(len(data.columns)).reshape(1, -1))
-                self.all_params += [search.best_params_]
+                # saves all the useful data
                 self.all_features += [feature_selection.transform(np.arange(len(data.columns)).reshape(1, -1))]
-                # print(self._best_features)
                 if verbose:
+                    # prints useful information if True
                     print(
                         f"accuracy: {self.test_accuracy[-1][0]}; "
-                        f"precision:{self.test_precision[-1][0]};"
-                        f" model:{search.best_estimator_}")
+                        f"precision: {self.test_precision[-1][0]};"
+                        f" model: {search.best_estimator_}")
+                # updates the progress bar
                 self.update_progress(progress=(i * self.outer_repeats + j + 1) / (self.outer_repeats * loop))
-        self.selected_features = self._best_features[0]
+        # stores all the data in to a pandas DataFrame
         self.train_accuracies = pd.DataFrame(self.train_accuracies, columns=self.indexes)
         self.train_precision = pd.DataFrame(self.train_precision, columns=self.indexes)
         self.train_recall = pd.DataFrame(self.train_recall, columns=self.indexes)
         self.test_accuracy = pd.DataFrame(self.test_accuracy, columns=["values"] + list(self.hyperParams.keys()))
         self.test_precision = pd.DataFrame(self.test_precision, columns=["values"] + list(self.hyperParams.keys()))
         self.test_recall = pd.DataFrame(self.test_recall, columns=["values"] + list(self.hyperParams.keys()))
-        self.fit(data, classes)
 
-    def __test__(self, model, data, classes):
+
+    def __test__(self, model: sklearn.model_selection.GridSearchCV, data, classes):
         """
         calculates all the accuracy, precision, and recall of a given model with data and classes
         :param model: an Sklearn model that is already fitted.
@@ -124,58 +114,6 @@ class DCV():
                                 list(model.best_params_.values())]
         self.test_recall += [[recall_score(classes, model.predict(data), average="macro", zero_division=0)] +
                              list(model.best_params_.values())]
-
-    def fit(self, data, classes, params: list = None, features: dict = None):
-        """
-        Fit
-
-        :param data:
-        :param classes:
-        :param params:
-        :param features:
-        :return:
-        """
-        if self._best_params == None and params == None:
-            raise RuntimeError("RuntimeError: you did not train yet. please use the Train_fit function.")
-        elif params != None:
-            self.Model.set_params(**params)
-        else:
-            print(self._best_params[0].values())
-            params = {x: list(self._best_params[0].values())[i] for i, x in enumerate(self.hyperParams)}
-            self.Model.set_params(**params)
-        data = self.__data_cleaner__(data)
-        ##################
-        if features == "all":
-            pass
-        elif len(self.selected_features) == 0 and features:
-            raise RuntimeError("RuntimeError: you did not train yet. please use the Train_fit function. features")
-        elif features != None:
-            data = data.iloc[:, features]
-        else:
-            print(self.selected_features)
-            data = data.iloc[:, self.selected_features]
-        ##################
-        classes = self.__class_cleaner__(classes)
-        self.Model.fit(data, classes.to_numpy().ravel())
-
-    def set_features(self, features: list):
-        """
-
-        :param features:
-        :return:
-        """
-        self.selected_features = features
-
-    def predict(self, data: pd.DataFrame) -> list:
-        """
-
-        :param data:
-        :return:
-        """
-        data = self.__data_cleaner__(data)
-        if self._best_features.any():
-            data = data.iloc[:, self._best_features[0]]
-        return self.Model.predict(data)
 
     def __data_cleaner__(self, data: pd.DataFrame or np.ndarray) -> pd.DataFrame:
         """
@@ -191,41 +129,28 @@ class DCV():
             return pd.DataFrame(data)
         raise TypeError("TypeError: data is not correct")
 
-    def __class_cleaner__(self, classes: pd.DataFrame or pd.Series or np.ndarray or pd.Index) -> pd.DataFrame:
+    def __class_cleaner__(self, classes: pd.core or np.ndarray) -> pd.DataFrame:
         """
-        cleanes the data that consist of the classes so that the script can use the data for most applications
+        cleans the data that consist of the classes so that the script can use the data for most applications
         :param classes: an array (pandas, numpy or pandas series) of all the classes.
         :return:
         returns the classes in a pandas array.
         """
-        if type(classes) == pd.core.indexes.base.Index:
+        if type(classes) == pd.core.indexes.base.Index or type(classes) == pd.core.indexes.range.RangeIndex:
+            return classes.to_frame()
+
+        elif type(classes) == np.ndarray:
+            return pd.DataFrame(classes)
+        elif type(classes) == pd.core.series.Series:
             return classes.to_frame()
         elif type(classes) == pd.DataFrame:
             return classes
-        elif type(classes) == np.ndarray:
-            return pd.DataFrame(classes)
-        elif type(classes) == pd.Series:
-            return classes.to_frame("classes")
         raise TypeError("TypeError: class data is not correct")
-
-    def get_best_model(self) -> sklearn.base.BaseEstimator:
-        self.Model = self.Model.set_params(**self._best_params[0])
-        return self.Model
-
-    def get_best_params(self) -> list:
-        return self._best_params
-
-    def get_best_features(self) -> list:
-        return self._best_features
-
-    def get_best_score(self) -> float:
-        return self._best_score
 
     def save_all_data(self, modelname: str = "model"):
         """
-
-        :param modelName:
-        :return:
+        saves all the data for analyses and figures.
+        :param modelname: the name that the files will receive before a static name.
         """
         self.test_accuracy.to_csv("{}_test_accuracy.csv".format(modelname))
         self.test_precision.to_csv("{}_test_precision.csv".format(modelname))
@@ -233,17 +158,16 @@ class DCV():
         self.train_accuracies.to_csv("{}_train_accuracy.csv".format(modelname))
         self.train_precision.to_csv("{}_train_precision.csv".format(modelname))
         self.train_recall.to_csv("{}_train_recall.csv".format(modelname))
-        pd.DataFrame(self._best_features).to_csv("{}_features.csv")
-        pd.DataFrame(self._best_params).to_csv("{}_features.csv")
+        pd.DataFrame([x[0] for x in self.all_features]).to_csv("{}_features.csv".format(modelname))
 
-    def __scoring__(self, model: sklearn.base.BaseEstimator, x: pd.DataFrame, y: np.array) -> dict:
+    def __scoring__(self, model: sklearn.base.BaseEstimator or sklearn.base.ClassifierMixin, x: pd.DataFrame, y: np.array) -> dict:
         """
-        scoring for the gridsearch. this function is used during the innerloop to create all the data.
+        scoring for the grid search. this function is used during the inner loop to create all the data.
         :param model: the model
         :param x: the data.
         :param y: the classes
         :return:
-        :returns the accuracy, precision, and recall for the gridsearch in a dict format
+        :returns the accuracy, precision, and recall for the grid search in a dict format
         """
         y_pred = model.predict(x)
         acc = accuracy_score(y_true=y, y_pred=y_pred)
@@ -323,18 +247,12 @@ if __name__ == '__main__':
     # first is the data used, seconds comes the classifications and than with loop the amount of loops you want to do
     # modeller.train_fit(data, data.index, loop=1)
     # some test code
-    print("accuracy:")
-    print(accuracy_score(modeller.__class_cleaner__(data.index).to_numpy().ravel(), modeller.predict(data)))
-    print()
-    print(topInfo.transpose().iloc[:-1].iloc[modeller.get_best_features()[0]])
-    print(modeller.get_best_score())
-    print(modeller.get_best_features())
-    print(pd.DataFrame(modeller.get_best_features()).replace(True, 1))
     print(modeller.train_accuracies)
     print(modeller.train_precision)
     print(modeller.train_recall)
     print(modeller.test_accuracy)
     print(modeller.test_precision)
     print(modeller.test_recall)
-    # print(modeller.save_all())
+    modeller.save_all_data("test")
     # modeller.fit(data,data.index,{"C":1,"l1_ratio":0.9})
+
